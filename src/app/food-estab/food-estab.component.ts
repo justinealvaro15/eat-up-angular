fac2ce06a13d5ee8252f3a3bba945c62ffdded21
Lois Anne Leal <lbleal1@up.edu.ph>
2018-11-22 01:05:04 +0800

    add imports encapsulation

--- src/app/food-estab/food-estab.component.ts
+++ src/app/food-estab/food-estab.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { Shop } from '../shops/shops.model';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
@@ -7,7 +7,8 @@ import { ShopsService } from '../shops/shops.service';
 @Component({
   selector: 'app-food-estab',
   templateUrl: './food-estab.component.html',
-  styleUrls: ['./food-estab.component.css']
+  styleUrls: ['./food-estab.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class FoodEstabComponent implements OnInit, OnDestroy {
   shop: Shop;
