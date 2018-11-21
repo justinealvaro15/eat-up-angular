fb0f29d2732c186f82a75e67fb188eb38bcb20be
Lois Anne Leal <lbleal1@up.edu.ph>
2018-11-22 01:07:14 +0800

    Update panelOpenState

--- src/app/food-estab/addtl-details/addtl-details.component.ts
+++ src/app/food-estab/addtl-details/addtl-details.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addtl-details.component.css']
 })
 export class AddtlDetailsComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit() {
