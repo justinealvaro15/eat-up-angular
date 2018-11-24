b5ac0ef5f01c5a00a1a4fdb19fe1b5314d72f10e
Ria Yangzon <42495426+hopefulRialist@users.noreply.github.com>
2018-11-24 12:56:15 +0800

    Add files via upload

--- src/app/home-search/home-search.component.ts
+++ src/app/home-search/home-search.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-search',
   templateUrl: './home-search.component.html',
-  styleUrls: ['./home-search.component.scss']
+  styleUrls: ['./home-search.component.css']
 })
 export class HomeSearchComponent implements OnInit {
     //building or address
