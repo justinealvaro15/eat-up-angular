36adf3a3c389af5ff5c8ace5994321cde81a1c00
justinealvaro15 <jpalvaro@up.edu.ph>
2018-11-24 22:03:54 +0800

    Connected search box to database ver1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,13 +12,11 @@ import { HttpClientModule } from '@angular/common/http';
 import {MatExpansionModule} from '@angular/material/expansion';
 
 import { TrendingNewestComponent } from './home/trending-newest/trending-newest.component';
-import { TrendingComponent } from './home/trending-newest/trending/trending.component';
-import { NewestComponent } from './home/trending-newest/newest/newest.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
-import { SearchBoxesComponent } from './search-boxes/search-boxes.component';
+// import { SearchBoxesComponent } from './search-boxes/search-boxes.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { SearchResultsComponent } from './search-page/search-results/search-results.component';
 import { FoodEstabComponent } from './food-estab/food-estab.component';
@@ -31,22 +29,22 @@ import { ReviewsMenuComponent } from './food-estab/reviews-menu/reviews-menu.com
 import { MenuComponent } from './food-estab/reviews-menu/menu/menu.component';
 import { ReviewsComponent } from './food-estab/reviews-menu/reviews/reviews.component';
 import { ShopCardComponent } from './home/shop-card/shop-card.component';
-import { MapComponent } from './map/map.component';
-import { HomeSearchComponent } from './home-search/home-search.component';
+// import { MapComponent } from './map/map.component';
+// import { HomeSearchComponent } from './home-search/home-search.component';
 import { FcsSearchPipe } from './fcs-search.pipe';
 import { LocationSearchPipe } from './location-search.pipe';
+import { HomeSearchComponent } from './home/search-boxes/search-boxes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrendingNewestComponent,
-    TrendingComponent,
-    NewestComponent,
     HomeComponent,
     HeaderComponent,
     SidenavListComponent,
     SignInComponent,
-    SearchBoxesComponent,
+    HomeSearchComponent,
+    // SearchBoxesComponent,
     SearchPageComponent,
     SearchResultsComponent,
     FoodEstabComponent,
@@ -59,12 +57,13 @@ import { LocationSearchPipe } from './location-search.pipe';
     MenuComponent,
     ReviewsComponent,
     ShopCardComponent,
-    MapComponent,
-    HomeSearchComponent,
+    // MapComponent,
+    // HomeSearchComponent,
     FcsSearchPipe,
-    LocationSearchPipe,
+    LocationSearchPipe
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
@@ -79,7 +78,8 @@ import { LocationSearchPipe } from './location-search.pipe';
     FlexLayoutModule,
     MatCardModule,
     HttpClientModule,
-    MatExpansionModule
+    MatExpansionModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/trending-newest/trending-newest.component.ts
+++ src/app/home/trending-newest/trending-newest.component.ts
@@ -10,9 +10,12 @@ import { Subscription } from 'rxjs';
   encapsulation: ViewEncapsulation.None
 })
 export class TrendingNewestComponent implements OnInit, OnDestroy {
-  newestShops: Shop[];
-  trendingShops: Shop[];
+  newestShops: Shop[] = [];
+  trendingShops: Shop[] = [];
+  shops: Shop[] = []
   subscriptions: Subscription[] = [];
+  private nameFilter: string;
+  private locationFilter: string;
 
 
   constructor(
@@ -36,4 +39,49 @@ export class TrendingNewestComponent implements OnInit, OnDestroy {
     })
   }
 
+  onNameFilterChange(name) {
+    this.nameFilter = name;
+  }
+
+  onLocationFilterChange(location) {
+    this.locationFilter = location;
+  }
+
+  getFilteredShops(shops: Shop[]): Shop[] {
+    return shops.filter((shop) => {
+      return this.isLocationMatch(shop) && this.isNameMatch(shop);
+    });
+  }
+
+  private isLocationMatch(shop: Shop): boolean {
+    if (!this.locationFilter) {
+      return true;
+    }
+
+    return shop.address.toLowerCase().includes(this.locationFilter.toLowerCase());
+  }
+
+  private isNameMatch(shop: Shop): boolean {
+    if (!this.nameFilter) {
+      return true;
+    }
+
+    const isFoodEstabName = shop.fe_name.toLowerCase().includes(this.nameFilter.toLowerCase());
+
+    const hasConsumableInFoodEstab = this.isConsumableInFoodEstab(shop);
+
+    return isFoodEstabName || hasConsumableInFoodEstab;
+  }
+
+  private isConsumableInFoodEstab(shop: Shop): boolean {
+    let hasConsumable = false
+
+    shop.Consumables.forEach((consumable) => {
+      if(consumable.c_name.toLowerCase().includes(this.nameFilter.toLowerCase())) {
+        hasConsumable = true;
+      }
+    });
+
+    return hasConsumable;
+  }
 }
