4cbc44122c44415b89fcf7e920eab0b54240b03c
justinealvaro15 <jpalvaro@up.edu.ph>
2018-11-24 22:27:15 +0800

    Updated display of ratings and status in Home

--- src/trend_newest_dummy_classes/shops_temp.ts
+++
@@ -1,9 +0,0 @@
-import { ImgSrcDirective } from "@angular/flex-layout";
-
-export class shops_temp{
-    name: string;
-    shopImg: string;
-    ratings: number;
-    address: string; 
-    ifopen: string;
-}
