04a6f9d009dadc58094be8be32063cfb3e48e35f
justinealvaro15 <jpalvaro@up.edu.ph>
2018-11-25 22:39:41 +0800

    Added Search Page Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { TrendingNewestComponent } from './home/trending-newest/trending-newest.component';
 import { FoodEstabComponent } from './food-estab/food-estab.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/shops', pathMatch: 'full' },
   { path: 'shops', component: TrendingNewestComponent },
+  { path: 'search', component: SearchPageComponent },
   { path: 'shops/:shopId', component: FoodEstabComponent },
   { path: 'sign-in', component: SignInComponent },
 ];
