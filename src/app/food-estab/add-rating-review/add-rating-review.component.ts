62fe5d6e57a097311fc226da53a9c2a10cc1ec03
Lois Anne Leal <lbleal1@up.edu.ph>
2018-11-28 19:59:55 +0800

    added script for dialog

--- src/app/food-estab/add-rating-review/add-rating-review.component.ts
+++ src/app/food-estab/add-rating-review/add-rating-review.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject,ViewEncapsulation} from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
+export interface DialogData {
+  rating:number;
+  review: string;
+  shopName: string;
+}
 @Component({
   selector: 'app-add-rating-review',
   templateUrl: './add-rating-review.component.html',
-  styleUrls: ['./add-rating-review.component.css']
+  styleUrls: ['./add-rating-review.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AddRatingReviewComponent implements OnInit {
 
-  constructor() { }
+/**
+ * @title Dialog Overview
+ */
 
-  ngOnInit() {
+export class AddRatingReviewComponent {
+  rating: number;
+  review: string;
+  shopName: string;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(AddRatingReviewDialog, {
+      width: '500px',
+      data: {shopName: this.shopName, rating:this.rating, review: this.review}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.review = result;
+    });
+
+    
+  }
+ 
+}
+
+
+@Component({
+  selector: 'add-rating-review-dialog',
+  templateUrl: 'add-rating-review-dialog.html',
+})
+export class AddRatingReviewDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<AddRatingReviewDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
