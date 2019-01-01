af502c87b2aab18744b279fbda48d2be9e642f99
Ria Yangzon <42495426+hopefulRialist@users.noreply.github.com>
2019-01-02 00:48:43 +0800

    Add files via upload

---
+++ src/app/navigation/sidenav-list/google-auth/google-auth.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+ 
+import { AuthService } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+import { LoginOpt } from "angularx-social-login";
+
+const googleLoginOptions: LoginOpt = {
+  scope: 'profile email'
+}; // https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiauth2clientconfig
+
+
+@Component({
+  selector: 'app-google-auth',
+  templateUrl: './google-auth.component.html',
+  styleUrls: ['./google-auth.component.css']
+})
+
+export class GoogleAuthComponent implements OnInit {
+
+  private user: SocialUser;
+  private loggedIn: boolean;
+
+  constructor(private authService: AuthService) { }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  ngOnInit() {
+  	this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+}
