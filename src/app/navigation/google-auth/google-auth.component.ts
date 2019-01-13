36c69e94925fdc7f722d0022c80e112b622a18cc
Ria Yangzon <42495426+hopefulRialist@users.noreply.github.com>
2019-01-14 05:46:59 +0800

    Add files via upload

---
+++ src/app/navigation/google-auth/google-auth.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input } from '@angular/core';
+ 
+import { AuthService } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+import { LoginOpt } from "angularx-social-login";
+
+//https://www.npmjs.com/package/angularx-social-login
+
+const googleLoginOptions: LoginOpt = {
+  scope: 'profile email'
+}; // https://developers.google.com/api-client-library/javascript/reference/referencedocs#gapiauth2clientconfig
+
+
+@Component({
+  selector: 'app-google-auth',
+  templateUrl: './google-auth.component.html',
+  styleUrls: ['./google-auth.component.css']
+})
+
+export class GoogleAuthComponent implements OnInit {
+
+  private user: SocialUser;
+  private loggedIn: boolean;
+
+  //to know whether it was accessed from sidenav or header
+  @Input () sidenav: boolean;
+  @Input () header: boolean;
+
+
+  constructor(private authService: AuthService) { }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  ngOnInit() {
+  	this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+}
+
+/*use a service between this component to whatever component needs the user details (name and email and photoUrl) */
