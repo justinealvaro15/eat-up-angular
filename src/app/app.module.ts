cb544172382510bb518893778f9e40fbfc6abcef
Ria Yangzon <42495426+hopefulRialist@users.noreply.github.com>
2019-01-27 23:29:47 +0800

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,6 +47,10 @@ import { GoogleLoginProvider, LoginOpt} from "angularx-social-login";
 import { MapComponent } from './food-estab/map/map.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { UserProfileComponent } from './navigation/user-profile/user-profile.component';
+import {ArraySortPipe} from 'src/app/pipes/alphabetical-sort.pipe';
+import { TermsOfServiceComponent } from './navigation/terms-of-service/terms-of-service.component';
+import { PrivacyPolicyComponent } from './navigation/privacy-policy/privacy-policy.component';
+import { AboutUsComponent } from './navigation/about-us/about-us.component';
 
 const googleLoginOptions: LoginOpt = {
   client_id: "305506356766-0q3pes92ks9buimmcchbtek02f3an0oc",
@@ -65,22 +69,6 @@ const googleLoginOptions: LoginOpt = {
 export function provideConfig() {
   return config;
 }
-import { 
-  MatToolbarModule, 
-  MatButtonModule, 
-  MatSidenavModule, 
-  MatIconModule, 
-  MatListModule, 
-  MatTabsModule,
-  MatCardModule, 
-  MatProgressSpinnerModule,
-  MatDialogModule,
-  MatInputModule,
-  MatFormFieldModule,
-  MatSelectModule,
-  MatOptionModule,
-  
-} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -107,7 +95,11 @@ import {
     ReviewsDialog,
     GoogleAuthComponent,
     MapComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    ArraySortPipe,
+    TermsOfServiceComponent,
+    PrivacyPolicyComponent,
+    AboutUsComponent
   ],
   entryComponents: [
     AddRatingReviewDialog,
