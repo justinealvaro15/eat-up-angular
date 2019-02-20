40cfced69638d1e0e84a59df0c501a12d61bc598
makeithappenlois <lbleal1@up.edu.ph>
2019-02-20 23:00:07 +0800

    sidenav--user-profile

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -107,7 +107,7 @@ export function provideConfig() {
     PrivacyPolicyComponent,
     AboutUsComponent,
     TermsOfServicesComponent,
-
+    UserProfileComponent
   ],
   entryComponents: [
     AddRatingReviewDialog,
