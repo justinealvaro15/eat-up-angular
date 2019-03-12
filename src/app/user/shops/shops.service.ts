aeb15b9e05913931e71047fa1542075f49594f39
Ria Yangzon <42495426+hopefulRialist@users.noreply.github.com>
2019-03-12 08:55:57 +0800

    Add files via upload
    added changes for search location, search fcs, and search both location and fcs

--- src/app/user/shops/shops.service.ts
+++ src/app/user/shops/shops.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {  BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { Shop } from './shops.model';
@@ -30,19 +30,20 @@ export class ShopsService {
             lat: null
         }
     }
-    private locationCoors:Location;
+    private locationCoors: Location;
     private shops: Shop[] = [];
     private _shops: BehaviorSubject<Shop[]> = new BehaviorSubject<Shop[]>([]);
     filterChanged = new EventEmitter();
 
     filter = {
-        location: '',
+        location: 0,
         fcs: ''
     }
 
     setCoordinates(long: string, lat: string) {
         this.coordinate.long = long;
         this.coordinate.lat = lat;
+        //console.log("inSetCoordinates: "+ this.coordinate.lat + " " + this.coordinate.long);
     }
 
     getShopsDisplay() {
@@ -55,11 +56,11 @@ export class ShopsService {
         })
     }
 
-    getCoordinatesByLocationId(locationId: string) { 
-        return this.http.get<Shop|null>(`http://localhost:3000/api/location/${locationId}`).pipe( 
+    getCoordinatesByLocationId(locationId: string) {
+        return this.http.get<Shop | null>(`http://localhost:3000/api/location/${locationId}`).pipe(
             map((location: any) => {
                 // console.log(location[0].coordinates.long);
-                if(location.length > 0){
+                if (location.length > 0) {
                     this.setCoordinates(location[0].coordinates.long, location[0].coordinates.lat);
                 }
                 return location.length > 0 ? location[0].coordinates : null;
@@ -68,18 +69,18 @@ export class ShopsService {
     }
 
     getShopById(shopId: string) {
-        return this.http.get<Shop|null>(`http://localhost:3000/api/shops/${shopId}`).pipe(
+        return this.http.get<Shop | null>(`http://localhost:3000/api/shops/${shopId}`).pipe(
             map((shops: any) => {
                 return shops.length > 0 ? shops[0] : null;
             })
         );
     }
 
-    getShopByRating(){
-        return this.http.get<Shop[]>('http://localhost:3000/api/shops/topten'); 
+    getShopByRating() {
+        return this.http.get<Shop[]>('http://localhost:3000/api/shops/topten');
     }
 
-    getShopByNewest(){
+    getShopByNewest() {
         return this.http.get<Shop[]>('http://localhost:3000/api/shops/newest');
     }
 
@@ -99,7 +100,7 @@ export class ShopsService {
     }
 
     addFoodOrBeverageByShopId(shopId: string, addedMenu: AddedMenu) {
-        const payload =  {
+        const payload = {
             user: this.user,
             addedMenu
         }
@@ -108,63 +109,117 @@ export class ShopsService {
 
 
     getFilteredShops(): Shop[] { //has to be modified
-        //get FCS Matches first then order from nearest to farthest
-        return this._shops.getValue().filter((shop) => {
-            // console.log(shop);
-            return this.isLocationMatch(shop) && this.isFCSMatch(shop);
-        });
-    }
+         //get FCS Matches first then order from nearest to farthest
+         if (this.filter.fcs && this.filter.location) { //both location and fcs
+            const filteredShops = this._shops.getValue().filter((shop)=> {
+                return this.isFCSMatch(shop);
+            });
+            return this.isArrayShopNearBldg(filteredShops);
+         }
+         else { //either location and fcs only
+                return this._shops.getValue().filter((shop) => {
+                    // console.log(shop)    
+                    if (this.filter.fcs) { //fcs search only
+                       return this.isFCSMatch(shop);
+                    }
+                    else if (this.filter.location != 0){ //location only 
+                        return this.isShopNearBldg(shop);
+                    }
+                });
+         }
+        
+     }
 
-    private isLocationMatch(shop: Shop): boolean { 
+    private getNearestShops(...args) { //any number of parameters (in this case 0 or 1)
         /* Takes the chosen location's coordinates(LC) from getCoordinatesbyLocationId (how to access it to here)
-            Takes all shops coordinates (SC) from shop service getShopsCoordinates
-            Applies Euclidean Distance (ED) on all SC with LC
-            Stores the resulting ED in an array of [(shop id, ED),... ] in order of lowest to highest
-            returns list of shops in that order */
-          //  ]
-        var locationCoors = this.getCoordinatesByLocationId(this.filter.location); //need to get the return
-        var shopsCoor = shop.coordinates;//this.getShopsDisplay(); //to get an Array of Shops Coordinates (is it possible to limit the returned value to shopId and coordinates only?)
-        //how to get shops
-        var distanceArr;
-        // console.log("lc:");
-        //  console.log(locationCoors);
-        // console.log("sc:");
-        // console.log(typeof shopsCoor);
-     
-  
-        //Euclidean Distance
-        //loop over all shops
-            //var distance  = Math.hypot(locationCoor.lat - shop.lat , locationCoor.long - shop.long);
-            //var shopscoor = [shopId, distance]
-            //distanceArr.push(shopscoor)
-        //distanceArr.sort(function(a,b) {
-        //    return a[1] - b[1];
-        //})
-        //use distanceArr to sort how it should be ordered on result
-        return shop.address.toLowerCase().includes(this.filter.location.toLowerCase());
-    }
-
-    private isFCSMatch(shop: Shop): boolean {
-        if (!this.filter.fcs) {
-            return true;
+           Takes all shops coordinates (SC) from shop service getShopsCoordinates
+           Applies Euclidean Distance (ED) on all SC with LC
+           Stores the resulting ED in an array of [(shop id, ED),... ] in order of lowest to highest
+           returns list of shops in that order */
+        //  ]
+        var shops: any;
+        if (args) {
+            shops = this._shops.getValue(); //array of objects
+        } else {
+            shops = args;
+        }
+        //console.log(shops);
+        var shopDistance = []
+        var shopLat = 0;
+        var shopLong = 0;
+        for (var i = 0; i < shops.length; i++) {
+            shopLat = shops[i].coordinates.lat;
+            shopLong = shops[i].coordinates.long;
+            //[fe_id, Euclidean Distance between it and the bldg]
+            shopDistance.push([shops[i].fe_id, Math.hypot(Number(this.coordinate.lat) - shopLat, Number(this.coordinate.long) - shopLong)]);
         }
 
+
+        shopDistance.sort(function (a, b) {
+            return a[1] - b[1];
+        }); //sorted
+        //console.group(shopDistance);
+        // return this.sortShop(shop,[shopId, dist]);//{"shop": [shopId, dist]};
+        return 0;
+    }
+
+    private isArrayShopNearBldg(filteredShops: Shop []): Shop[] {
+        /* 
+            Takes an array of shops already filtered by isFCSMatch, filteredShops,
+            and figures out whether the chosen building of the user
+            is near one of those shops.
+        */
+        let arrayShopsNearBldg=[];
+        for (var i = 0;i<filteredShops.length;i++) {
+          
+           
+            filteredShops[i].Nearest_Bldgs.forEach((bldg)=>{
+                if (bldg.id == this.filter.location) {
+                    arrayShopsNearBldg.push(filteredShops[i]);
+                }
+            });
+            
+        }
+        return arrayShopsNearBldg;
+    }
+
+   
+
+    private isFCSMatch(shop: Shop): boolean{ //Food or Shop Match Only
+        //need to take into account trailing whitespace
+        if (!this.filter.fcs){
+            return true;
+        }
+    
         const isFoodEstabName = shop.fe_name.toLowerCase().includes(this.filter.fcs.toLowerCase());
 
-        const hasConsumableInFoodEstab = this.isConsumableInFoodEstab(shop);
-
+        const hasConsumableInFoodEstab = this.isConsumableInFoodEstab(shop); //not workin
+        console.log("IN isFCSMatch");
         return isFoodEstabName || hasConsumableInFoodEstab;
+       
+    }
+
+    private isShopNearBldg(shop: Shop): boolean { //Location Only 
+            let shopNearBldg = false;
+            shop.Nearest_Bldgs.forEach((bldg) => {
+                if (bldg.id == this.filter.location) {
+                    shopNearBldg = true;
+                }
+            });
+            console.log("isShopNearBldg "+ shopNearBldg);
+            return shopNearBldg;     
     }
 
     private isConsumableInFoodEstab(shop: Shop): boolean {
         let hasConsumable = false
 
         shop.Consumables.forEach((consumable) => {
-            if(consumable.c_name.toLowerCase().includes(this.filter.fcs.toLowerCase())) {
+            
+            if (consumable.c_name.toLowerCase().includes(this.filter.fcs.toLowerCase())) {
                 hasConsumable = true;
             }
         });
-
+        console.log(this.filter.fcs+" "+hasConsumable);
         return hasConsumable;
     }
 }
