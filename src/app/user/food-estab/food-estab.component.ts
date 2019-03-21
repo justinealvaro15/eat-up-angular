4fd6d981c168d1da65818dcd3dd597d737637726
justinealvaro15 <jpalvaro@up.edu.ph>
2019-03-21 23:17:34 +0800

    Disables "Add Menu" button when review exists

--- src/app/user/food-estab/food-estab.component.ts
+++ src/app/user/food-estab/food-estab.component.ts
@@ -4,6 +4,8 @@ import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { ShopsService } from '../shops/shops.service';
 import * as moment from 'moment';
+import { ReviewsService } from '../reviews/reviews.service';
+import { Review } from '../reviews/reviews.model';
 
 @Component({
   selector: 'app-food-estab',
@@ -14,21 +16,25 @@ import * as moment from 'moment';
 export class FoodEstabComponent implements OnInit, OnDestroy {
   shop: Shop;
   shopSubscription: Subscription;
+  reviews: Review[] = [];
 
   constructor(
     private route: ActivatedRoute,
-    private shopService: ShopsService
+    private shopService: ShopsService,
+    private reviewService: ReviewsService
   ) { }
 
   ngOnInit() {
     const shopId = this.route.snapshot.paramMap.get('shopId');
-
     this.shopSubscription = this.shopService.getShopById(shopId).subscribe(shop => this.shop = shop);
+    this.reviewService.getReviewsByNewest(shopId).subscribe((reviews) => {
+      this.reviews = reviews;
+    });
   }
 
   ngOnDestroy() {
     try {
-      this.shopSubscription.unsubscribe()
+      this.shopSubscription.unsubscribe();
     } catch { }
   }
 
