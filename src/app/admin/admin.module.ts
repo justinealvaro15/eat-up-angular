f4a9e5193c60ba6dc19b9b7c0923081d7b75d42e
Ria Yangzon <42495426+hopefulRialist@users.noreply.github.com>
2019-03-26 11:56:46 +0800

    Add files via upload

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,7 +6,7 @@ import { AdminRouting } from './admin-routing.module';
 import { AdminComponent} from './admin.component';
 
 /*main-pages*/
-import { AdminsComponent } from './main-pages/admins/admins.component';
+import {AdminsComponent} from './main-pages/admins/admins.component';
 import { UsersComponent } from './main-pages/users/users.component';
 import { FoodEstablishmentsComponent, AddShopDialog } from './main-pages/food-establishments/food-establishments.component';
 import { MenuComponent } from './main-pages/menu/menu.component';
@@ -17,7 +17,6 @@ import { NotificationsComponent } from './misc-pages/notifications/notifications
 
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
-
 import {
   MatButtonModule,
   MatInputModule,
@@ -26,9 +25,20 @@ import {
   MatTooltipModule,
   MatSelectModule,
   MatGridListModule,
-  MatIconModule
+  MatIconModule,
+  MatDividerModule,
+  MatListModule
 } from '@angular/material';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { StatsComponent } from './misc-pages/stats/stats.component';
+import { TableListComponent } from './table-list/table-list.component';
+import { TypographyComponent } from './typography/typography.component';
+import { UpgradeComponent } from './upgrade/upgrade.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AddAdminComponent } from './main-pages/admins/add-admin/add-admin.component';
+import { UserSearchComponent } from './main-pages/users/user-search/user-search.component';
+import { UserListComponent, MakeAdminDialog, DeacUserDialog } from './main-pages/users/user-list/user-list.component';
+import { UserCardComponent } from './main-pages/users/user-card/user-card.component';
 
 @NgModule({
   imports: [
@@ -43,11 +53,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     AdminRouting,
     RouterModule,
     MatGridListModule,
+    AddShopDialog,
     MatIconModule,
-    AddShopDialog
-  ],
-  entryComponents: [
-    AddShopDialog
+    MatDividerModule,
+    MatListModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AdminComponent,
@@ -58,6 +68,24 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     MenuComponent,
     SidebarComponent,
     NavbarComponent,
+    NotificationsComponent,
+    StatsComponent,
+    TableListComponent,
+    TypographyComponent,
+    UpgradeComponent,
+    UserProfileComponent,
+    AddAdminComponent,
+    UserSearchComponent,
+    UserListComponent,
+    MakeAdminDialog,
+    DeacUserDialog,
+    UserCardComponent
+
+  ],
+  entryComponents: [
+    MakeAdminDialog,
+    DeacUserDialog,
+    AddShopDialog
   ]
 })
 
