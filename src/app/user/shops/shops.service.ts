1927a5bb766b6fa149bf375d9c3e4e8f295b2c47
Ria Yangzon <42495426+hopefulRialist@users.noreply.github.com>
2019-03-26 11:06:51 +0800

    Add files via upload
    added .trim() to filters to remove user entered trailing and leading white space

--- src/app/user/shops/shops.service.ts
+++ src/app/user/shops/shops.service.ts
@@ -3,11 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { Shop, Consumables, BrandedConsumables } from './shops.model';
+import { Shop, Consumables } from './shops.model';
 import { Location } from '../location/location.model';
+import { AddedMenu } from '../food-estab/add-menu-item/add-menu-item.component';
 import { AuthService, SocialUser } from 'angularx-social-login';
 import { LocationService } from '../location/location.service';
-import { AddedMenu } from '../food-estab/food-group';
 
 @Injectable({ providedIn: 'root' })
 export class ShopsService {
@@ -89,7 +89,7 @@ export class ShopsService {
     }
 
     setFilter(key: FilterKeys, filterValue: string) {
-        this.filter[key] = filterValue;
+        this.filter[key] = filterValue.trim();
         this.filterChanged.emit(this.filter);
     }
 
@@ -104,27 +104,37 @@ export class ShopsService {
             user: this.user,
             addedMenu
         }
-        this.http.post(`http://localhost:3000/api/shops/${shopId}/food`, payload).subscribe();
+        this.http.post(`http://localhost:3000/api/shops/${shopId}/${addedMenu.group.toLowerCase()}`, payload).subscribe();
     }
 
-    editFoodOrBeverageByShopid(shopId: string, group: string, type: string, editedMenu: Consumables[] | BrandedConsumables[]) {
-        const payload = {
-            // user: this.user,
-            updatedMenu: {
-                group,
-                type
-            },
-            value: editedMenu
-        }
-        this.http.put(`http://localhost:3000/api/shops/${shopId}/food`, payload).toPromise().then((res) => { console.log(res); });
-    }
 
     getFilteredShops(): Shop[] { //has to be modified
         const fcsFiltered = this._shops.getValue().filter((shop)=> {
             return this.isFCSMatch(shop);
         });
         return this.isArrayShopNearBldg(fcsFiltered);
-    }
+
+
+         //get FCS Matches first then order from nearest to farthest
+         if (this.filter.fcs && this.filter.location) { //both location and fcs
+            const filteredShops = this._shops.getValue().filter((shop)=> {
+                return this.isFCSMatch(shop);
+            });
+            return this.isArrayShopNearBldg(filteredShops);
+         }
+         else { //either location and fcs only
+                return this._shops.getValue().filter((shop) => {
+                    // console.log(shop)    
+                    if (this.filter.fcs) { //fcs search only
+                       return this.isFCSMatch(shop);
+                    }
+                    else if (this.filter.location != 0){ //location only 
+                        return this.isShopNearBldg(shop);
+                    }
+                });
+         }
+        
+     }
 
     private getNearestShops(...args) { //any number of parameters (in this case 0 or 1)
         /* Takes the chosen location's coordinates(LC) from getCoordinatesbyLocationId (how to access it to here)
