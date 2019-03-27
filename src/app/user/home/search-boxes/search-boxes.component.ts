6b26fe85d52aa48959b16faf8db562b4a2569168
Ria Yangzon <mlyangzon@up.edu.ph>
2019-03-28 02:17:32 +0800

    removed trim()

--- src/app/user/home/search-boxes/search-boxes.component.ts
+++ src/app/user/home/search-boxes/search-boxes.component.ts
@@ -360,8 +360,9 @@ export class HomeSearchComponent implements OnInit {
 
 
   setFiltersAndRedirect() {
-    this.shopService.setFilter(FilterKeys.Location, this.filter.get('location').value.trim());
-    this.shopService.setFilter(FilterKeys.FCS, this.filter.get('fcs').value.trim());
+  
+    this.shopService.setFilter(FilterKeys.Location, this.filter.get('location').value);
+    this.shopService.setFilter(FilterKeys.FCS, this.filter.get('fcs').value);
 
     if (this.route.snapshot.url.toString() !== 'search') {
       this.router.navigate(['/user/search']);
