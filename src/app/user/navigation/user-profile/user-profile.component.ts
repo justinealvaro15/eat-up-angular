1a7f5cb1e64af7bf4cfdc6efa11f59b4d34e5fc5
Ria Yangzon <mlyangzon@up.edu.ph>
2019-03-28 09:50:30 +0800

    gets number of reviews a user has made

--- src/app/user/navigation/user-profile/user-profile.component.ts
+++ src/app/user/navigation/user-profile/user-profile.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService, SocialUser } from 'angularx-social-login';
+import { UsersService } from '../../../admin/main-pages/users/users.service';
+import { ReturnStatement } from '@angular/compiler';
+import { User } from '../../../admin/main-pages/users/user.model';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,14 +12,23 @@ import { AuthService, SocialUser } from 'angularx-social-login';
 export class UserProfileComponent implements OnInit {
   public user: SocialUser;
   public loggedIn: boolean;
-
-  constructor(private authService: AuthService) { }
+  public numReviews: any;
+  public loggedInUser: User[];
+  constructor(private authService: AuthService,
+    private usersService: UsersService) { }
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
       this.user = user;
       this.loggedIn = (user != null);
+      this.numReviews = this.getUserNumReviews(this.user.email)[0].reviews_made;
     });
   }
-
+  getUserNumReviews(email) {
+    this.loggedInUser = this.usersService.getFilteredUsers().filter(user =>{
+        return user.email == email;
+    });
+    console.log(this.loggedInUser);
+    return this.loggedInUser;
+  }
 }
